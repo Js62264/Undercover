a27312311619a469a01cf5296f11901fe1274ed1
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-07 03:54:33 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -2,7 +2,6 @@ import re
 from pyrogram import filters, Client
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
 from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup 
 from database.ia_filterdb import unpack_new_file_id
 from utils import temp
 import re
@@ -13,7 +12,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 
 @Client.on_message(filters.command('link') & filters.user(ADMINS))
 async def gen_link_s(bot, message):
@@ -28,26 +26,40 @@ async def gen_link_s(bot, message):
     
     
 @Client.on_message(filters.command('batch') & filters.user(ADMINS))
-async def gen_link_batch(bot:Client, message:Message):
-    user_id = message.from_user.id 
-    post1:Message = await bot.ask(chat_id=message.chat.id, text="Please Forward The First Post From The Channel (with Quotes)..", timeout=360) 
-    if not post1:
-      return
-    if not post1.forward_from_chat:
-      await message.reply_text("Please Forward The Message With Quotes (ie : Forwarded From ...)")
-      return
-    
-    f_chat_id = post1.forward_from_chat.id 
- 
-    post2 = await bot.ask(chat_id=message.chat.id, text="Now Forward The Last Message From The Same Channel", timeout=360) 
-    if not post2 :
-      return 
- 
-    l_chat_id = post2.forward_from_chat.id
-    
-    if not f_chat_id==l_chat_id : 
-        return await message.reply_text("These Two Messages Arent From The Same Chat") 
-   
+async def gen_link_batch(bot, message):
+    if " " not in message.text:
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+    links = message.text.strip().split(" ")
+    if len(links) != 3:
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/TeamEvamaria/10 https://t.me/TeamEvamaria/20</code>.")
+    _, first, last = links
+    regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+    match = regex.match(first)
+    if not match:
+        return await message.reply('Invalid link')
+    f_chat_id = match.group(4)
+    f_msg_id = int(match.group(5))
+    if f_chat_id.isnumeric():
+        f_chat_id  = int(("-100" + f_chat_id))
+
+    match = regex.match(last)
+    if not match:
+        return await message.reply('Invalid link')
+    l_chat_id = match.group(4)
+    l_msg_id = int(match.group(5))
+    if l_chat_id.isnumeric():
+        l_chat_id  = int(("-100" + l_chat_id))
+
+    if f_chat_id != l_chat_id:
+        return await message.reply("Chat ids not matched.")
+    try:
+        chat_id = (await bot.get_chat(f_chat_id)).id
+    except ChannelInvalid:
+        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+    except (UsernameInvalid, UsernameNotModified):
+        return await message.reply('Invalid Link specified.')
+    except Exception as e:
+        return await message.reply(f'Errors - {e}')
 
     sts = await message.reply("Generating link for your message.\nThis may take time depending upon number of messages")
     if chat_id in FILE_STORE_CHANNEL:
