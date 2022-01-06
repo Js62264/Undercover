0666ce398d412e106728f52c1f814f162e9ea7b3
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-07 01:23:30 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -64,38 +64,79 @@ async def batch(bot:Client, update:Message):
     if not msg_id1<=msg_id2: 
         return await update.reply_text("The First Message Has To Be Posted Above The Second In The Channel To Generate A Batch") 
     
+    sts = await update.reply("Generating link for your message.\nThis may take time depending upon number of messages")
     
-    rng = [msg_id1]
-    Files = []
-    try:
-     for i in range(msg_id1, (msg_id2+1)):
-      xx = await bot.copy_message(chat_id = bot.db_channel.id, from_chat_id=chat_id2,message_id = i, disable_notification=True)
-      await asyncio.sleep(0.5)
-      Files.append(xx)
-    except MediaEmpty:
-     pass
+    if chat_id1 in FILE_STORE_CHANNEL:
+     string = f"{msg_id1}_{msg_id2}_{chat_id}"
+     b_64 = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
+     return await sts.edit(f"Here is your link https://t.me/{temp.U_NAME}?start=DSTORE-{b_64}")
+   
+    msgs_list = []
+    c_msg = msg_id1
+    diff = msg_id2 - msg_id1
     
-    except FloodWait as e:
-     await asyncio.sleep(e.x)
+    FRMT = "Generating Link...\nTotal Messages: `{total}`\nDone: `{current}`\nRemaining: `{rem}`\nStatus: `{sts}`"
+    if diff <= 200:
+     msgs = await bot.get_messages(chat_id1, list(range(msg_id1, msg_id2+1)))
+     msgs_list += msgs
+    
+    else:
+     c_msg = msg_id1
+     while True:
+      new_diff = msg_id2 - c_msg
+      if new_diff > 200:
+       new_diff = 200
+      elif new_diff <= 0:
+       break
+      print(new_diff, c_msg)
+      msgs = await bot.get_messages(f_chat_id, list(range(c_msg, c_msg+new_diff)))
+      msgs_list += msgs
+      try:
+       await sts.edit(FRMT.format(total=diff, current=(c_msg - f_msg_id), rem=(l_msg_id - c_msg), sts="Fetching Messages"))
+      except:
+       pass
+      c_msg += new_diff
      
-    except Exception as e:
-     pass
+    outlist = []
+    og_msg = 0
+    tot = 0
+    for msg in msgs_list:
+     tot += 1
+     if msg.empty or msg.service:
+      continue
+     if not msg.media:
+      continue
+     try:
+      file_type = msg.media
+      file = getattr(msg, file_type)
+      if file:
+       file = {
+        "file_id": file.file_id,
+        "caption": msg.caption,
+        "title": getattr(file, "file_name", ""),
+        "size": file.file_size,
+       }
+       og_msg +=1
+       outlist.append(file)
       
-    #for i in range(msg_id1, (msg_id2+1)):
-     #xx = await bot.copy_message(chat_id = bot.db_channel.id, from_chat_id=chat_id2,message_id = i, disable_notification=True)
-     #await asyncio.sleep(1)
-     #Files.append(xx)
-    
-    converted_id1 = Files[0].message_id * abs(bot.db_channel.id)
-    converted_id2 = Files[-1].message_id * abs(bot.db_channel.id)
+      except:
+       pass
+      if not og_msg % 20:
+       try:
+        await sts.edit(FRMT.format(total=diff, current=tot, rem=(diff - tot), sts="Saving Messages"))
+       except:
+        pass
+      
      
-
-    string = f"get-{converted_id1}-{converted_id2}"
-    base64_string = await encode(string)
-    url = f"https://t.me/SpaciousUniverseBot?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={url}')]])
-    await update.reply_text(f"<b>➭ Here is your link : </b>\n\n{url}", reply_markup=reply_markup, disable_web_page_preview = True)
-    await bot.send_message(chat_id = client.db_channel.id, text=f"<b>➭ Here is your link :</b>\n\n{url}", disable_notification=True)
+     
+    with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
+     json.dump(outlist, out)
+    post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
+    os.remove(f"batchmode_{message.from_user.id}.json")
+    file_id, ref = unpack_new_file_id(post.document.file_id)
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+    
+    
     
     
    
