01b31775ab319a4a77cd7f9127c2e5b6133f8d3e
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-10 03:00:28 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -139,4 +139,4 @@ async def gen_link_batch(bot:Client, message:Message):
     post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
-    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}", disable_web_page_preview=True)
+    await sts.edit(f"Here is your link\nContains `{og_msg}` files.\nhttps://t.me/{temp.U_NAME}?start=BATCH-{file_id}", disable_web_page_preview=True)
