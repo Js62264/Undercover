5705ddc4849c1327e3460795de16cc52c5774b24
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-09 19:38:51 +0530

    Create Quote.py

---
+++ assets/Quote.py
@@ -0,0 +1,9 @@
+class quote(object):
+  MARADONA_TXT = """
+○ Title: Maradona: Blessed Dream
+○ Year: 2021
+○ Language: English | Multi (Hindi Telghu Malayalam) 
+○ Season: Season 01
+    
+Choose the language you want..
+"""
