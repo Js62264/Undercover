88ad0b8cc07497ea5a67261c1c5003e8bc4b1f2e
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-10 12:30:40 +0530

    Update picture.py

--- assets/picture.py
+++ assets/picture.py
@@ -1,2 +1,3 @@
 MARADONA_PIC = 'https://telegra.ph/file/559f7dda0591da0b8e8bf.jpg'
 MONEY_HEIST_PIC = 'https://telegra.ph/file/fd47722be5369b68d1c94.jpg'
+VIKINGS_PIC = 'https://telegra.ph/file/2f26ddcebf5580f48c8f1.jpg'
