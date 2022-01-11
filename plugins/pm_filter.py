bbd9be351d9a6739b706b874acc6a5c340198391
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-12 02:04:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1703,6 +1703,8 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         cap = f"Here is what i found for your Request {search}"
+    if len(cap) > 1024:
+        cap = f"Here is what i found for your Request {search}"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
