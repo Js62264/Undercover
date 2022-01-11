09eb5ec476a2526c901a34677985a6b32e9f5ad1
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-12 02:16:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1703,12 +1703,14 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         cap = f"Here is what i found for your Request {search}"
-    if len(cap) > 1024:
-        cap = f"Here is what i found for your Request {search}"
-        return await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+        
     if imdb and imdb.get('poster'):
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            if len(cap) > 1024:
+               cap = f"Here is what i found for your Request {search}"
+               await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            else:
+               await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
