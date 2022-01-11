04aabe43e70c38eaf07eef20a1e82a3b6b6eeeb4
MiiNiiX <96678838+MiiNiiX@users.noreply.github.com>
2022-01-12 04:46:01 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -5,9 +5,13 @@ Import ast
 from pyrogram.errors import UserBannedInChannel, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters
-from ..plugins.pm_filter import *
+from plugins.pm_filter import *
 import logging
 logger = logging.getLogger(__name__)
+
+from Script import script
+from assets.picture import *
+from assets.Quote import quote
 
 
 @Client.on_callback_query()
