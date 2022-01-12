98089c097762645040d6f038a38f652db27964c8
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-13 02:22:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1667,6 +1667,9 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="Check PM", url ='https://t.me/SpaciousUniverseBot'), InlineKeyboardButton(text="》",callback_data=f"next_{req}_{key}_{offset}")]
         )
+        btn.append(
+            [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_{req}_{key}_{offset}")]
+        )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1",callback_data="pages"), InlineKeyboardButton(text="Check PM", url ='https://t.me/SpaciousUniverseBot'),]
