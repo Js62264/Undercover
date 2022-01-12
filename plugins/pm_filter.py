7b2690318fc0284861c9796995ce18595b547fe1
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-13 03:43:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -490,7 +490,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     file_id=file_id,
                     caption=f_caption
                     )
-                await query.answer('Check My Privet Chat, I have sent files to You',show_alert = True)
+                await query.answer('Check My Privet Chat, I have sent files to You')
         except UserIsBlocked:
             await query.answer('You Blocked Me!!. Start Me In privet chat and try Again.',show_alert = True)
         except PeerIdInvalid:
