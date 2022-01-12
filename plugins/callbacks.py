2f223da9c5994bf6bc0c87d7628a628007e9b430
MiiNiiX <96678838+MiiNiiX@users.noreply.github.com>
2022-01-12 13:07:00 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -16,3 +16,20 @@ from assets.Quote import quote
 
 @Client.on_callback_query()
 async def cb_handler(client, query):
+  if query.data == "stranger_things_H_480p":
+        buttons= [
+           [
+              InlineKeyboardButton('Season 01', url='https://t.me/SpaciousUniverseBot?start=BATCH-BQADBQADXwQAAl3T6FbdhkzTtRmN8xYE'),
+              InlineKeyboardButton('Season 02', url='https://t.me/SpaciousUniverseBot?start=BATCH-BQADBQADYAQAAl3T6FbMlJE3uvCd1xYE')
+           ],
+           [
+              InlineKeyboardButton('Season 03', url='https://t.me/SpaciousUniverseBot?start=BATCH-BQADBQADYQQAAl3T6FZy54s5vRvKcxYE'),
+              InlineKeyboardButton('Close', callback_data='cls')
+           ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=quote.RESULT_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
