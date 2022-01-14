955a5067c5c0f8ac768793d876abc447e80c1d98
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-15 04:18:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -204,12 +204,12 @@ async def next_page(bot, query):
          
     if SINGLE_BUTTON:
         btn = [
+            [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}┆{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
-           
         ]
             
     else:
@@ -1647,13 +1647,14 @@ async def auto_filter(client, msg, spoll=False):
     nameb = file.file_name
 
     if SINGLE_BUTTON:
+        if SINGLE_BUTTON:
         btn = [
+            [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}┆{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
-           
         ]
     else:
         btn = [
