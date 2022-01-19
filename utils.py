e283504f03a0806fdb0604b8679a961ae1a81f24
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-19 09:57:37 +0530

    Update Button Name (#92)

--- utils.py
+++ utils.py
@@ -196,6 +196,7 @@ def get_name(name):
     name = name.replace("Bullmoviee", '')
     name = name.replace("massmovies", '')
     name = name.replace("TIF", '')
+    name = name.replace("F&T", '')
     name = name.replace("FBM", '')
     name = name.replace("mwkOTT", '')
     name = name.replace("Team_HDT", '')
