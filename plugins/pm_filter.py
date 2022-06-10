1988abc1408ff895b0c06f8b0b032a8f241b584e
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-06-11 02:05:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -29,6 +29,13 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
+@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
+async def give_filter(client, message):
+    if message.text.startswith("/"):
+        return    
+    name = message.text
+    await auto_filter(client, message)
+
 @Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
 async def give_filter(client,message):
     name = message.text
